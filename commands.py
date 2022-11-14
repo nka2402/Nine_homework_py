35b1e08c3bebc4545e268d13bdd48482ab1d9b55
KabanJkee <KabanJkee@gmail.com>
2022-11-14 21:05:51 +0300

    Init comit

---
+++ commands.py
@@ -0,0 +1,77 @@
+import asyncio
+import random
+
+from create_bot import dp
+from aiogram import types
+
+import model
+from create_bot import bot
+
+
+async def start(message: types.Message):
+    player = message.from_user
+    model.set_player(player)
+    await bot.send_message(message.from_user.id, f'{message.from_user.first_name}, привет!'
+                                                 f'Сегодня будем делить конфеты')
+    await asyncio.sleep(3)
+    dp.register_message_handler(player_turn)
+    first_turn = random.randint(0,1)
+    if first_turn:
+        await await_player(player)
+    else:
+        await enemy_turn(player)
+
+async def player_turn(message: types.Message):
+    player = message.from_user
+    model.set_player(player)
+    if (message.text).isdigit():
+        if int(message.text) < 29:
+            total_count = model.get_total_candies()
+            player_take = int(message.text)
+            total = total_count - player_take
+            await bot.send_message(player.id, f'{player.first_name} взял {player_take} конфет, '
+                                              f'и на столе осталось {total}')
+            if model.check_win(total):
+                await bot.send_message(player.id, f'Победил {player.first_name}')
+                return
+            model.set_total_candies(total)
+            await enemy_turn(player)
+        elif int(message.text) < 0:
+            await bot.send_message(message.from_user.id, 'Это слишком мало!')
+        else:
+            await bot.send_message(message.from_user.id, 'Это слишком много!')
+    else:
+        await bot.send_message(message.from_user.id, f'{message.from_user.first_name}, '
+                                                     f'вообще-то мы конфеты считаем в цифрах')
+
+async def enemy_turn(player):
+    total_count = model.get_total_candies()
+    if total_count < 29:
+        enemy_take = total_count
+    else:
+        enemy_take = (total_count - 1)%28
+    total = total_count - enemy_take
+    await bot.send_message(player.id, f'Бот взял {enemy_take} конфет, '
+                                      f'и на столе осталось {total}')
+    if model.check_win(total):
+        await bot.send_message(player.id, f'{player.first_name} ты проиграл,'
+                                          f'попробуй еще раз!')
+        return
+    model.set_total_candies(total)
+    await asyncio.sleep(1)
+    await await_player(player)
+
+
+
+
+async def await_player(player):
+    max_take = model.get_max_take()
+    await bot.send_message(player.id,
+                           f'{player.first_name}, бери конфеты, но не больше {max_take}')
+
+async def set_total_candies(message: types.Message):
+
+    count = int((message.text).split(" ")[1])
+    model.set_total_candies(count)
+    await bot.send_message(message.from_user.id, f'Максимально количество конфет изменили на'
+                                                 f' {count}')
---
+++ create_bot.py
@@ -0,0 +1,8 @@
+
+
+
+from aiogram import Bot, Dispatcher
+
+
+bot = Bot(token='5632909893:AAF7BeNnHLEwDjRnkwC6rbjjWekZlNXfew0')
+dp = Dispatcher(bot)
